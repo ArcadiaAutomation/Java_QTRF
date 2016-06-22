2a5a82fb23cba0b4f0cd735ff0c2ff15ce8e342a
sakon777 <sakon778@hotmail.com>
2016-06-22 11:48:05 +0700

    first initial from Kit

---
+++ CoreFramework/Iteration.java
@@ -0,0 +1,23 @@
+package CoreFramework;
+
+import java.util.ArrayList;
+
+public class Iteration {
+
+	private static ArrayList<ArrayList<String>> table = new ArrayList<ArrayList<String>>();
+	
+	public static boolean isIteration(String path,String sheetName)
+	{
+		return ExcelManager.isSheetExist(path,sheetName);
+	}
+	
+	public static void readIteration(String testCasePath,String sheetName)
+	{
+		table = ExcelManager.getArray(testCasePath,sheetName);
+	}
+	
+	public static ArrayList<ArrayList<String>> getIteration()
+	{
+		return table;
+	}
+}
