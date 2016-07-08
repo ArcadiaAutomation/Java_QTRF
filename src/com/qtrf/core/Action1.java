f3646295a71517d60f1979a2189cdcfd3ad2142e
sakon777 <sakon778@hotmail.com>
2016-07-08 20:51:35 +0700

    - remove action1

--- src/com/qtrf/core/Action1.java
+++ src/com/qtrf/core/Action1.java
@@ -2,16 +2,15 @@ package com.qtrf.core;
 
 import org.junit.runner.JUnitCore;
 
-import com.qtrf.mobile.application.DriverManager;
 
 public class Action1 {
 
 	public static void main(String[] args) {
 		
-		LogManager.generateLog("001X1", "001X1", "001X1");
-		JUnitCore core = new JUnitCore(); 
-		core.run(DriverManager.class);
-		
+//		LogManager.generateLog("001X1", "001X1", "001X1");
+//		JUnitCore core = new JUnitCore(); 
+//		core.run(DriverManager.class);
+//		
 	
 	}
 }
